5925ad7aa786554928d7c595f319e15385bc393f
Gabriel Lenon <glenonsilva@gmail.com>
2020-05-04 01:02:38 -0300

    changes in models

--- core/models.py
+++ core/models.py
@@ -38,7 +38,9 @@ MEETING_CATEGORY_OPTIONS = [
     ('domingo', 'Culto de Domingo'),
     ('ceia', 'Ceia do Senhor'),
     ('casa', 'Cultuando em casa'),
-    ('infantil', 'Culto Infantil')
+    ('infantil', 'Culto Infantil'),
+    ('oracao', 'Ciclo de Oração'),
+    ('domestico', 'Culto doméstico'),
 ]
 
 MEMBERS_UNION_OPTIONS = [
@@ -164,7 +166,8 @@ class Schedule(models.Model):
     end_date = models.DateTimeField('Horário de fim', null=True, blank=True)
     location = models.ForeignKey('core.Church', verbose_name='Local', null=True, blank=True, on_delete=models.SET_NULL)
     description = models.TextField('Descrição', max_length=300, blank=True, null=True)
-    preacher = models.ForeignKey('core.Member', verbose_name='Pregador', null=True, on_delete=models.SET_NULL)
+    preacher = models.ForeignKey('core.Member', verbose_name='Pregador', related_name='pregador', blank=True, null=True, on_delete=models.SET_NULL)
+    leader = models.ForeignKey('core.Member', verbose_name='Pregador', related_name='dirigente', blank=True, null=True, on_delete=models.SET_NULL)
     organizing_group = models.ForeignKey('groups.Group', verbose_name='Grupo Organizador', null=True, blank=True, on_delete=models.SET_NULL)
     category = models.CharField('Tipo', max_length=15, choices=[(meetingType.name, meetingType.value) for meetingType in MeetingTypeEnum])
     video = models.ForeignKey('core.Video', verbose_name='Vídeo', null=True, blank=True, on_delete=models.SET_NULL)
