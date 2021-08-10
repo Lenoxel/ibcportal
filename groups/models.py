f25761b05ef1d56d91355edc1bc8f9084ebe089e
Lenoxel <glenonsilva@gmail.com>
2021-08-10 09:49:17 -0300

    :rocket: Adicionando alguns campos nos models

--- groups/models.py
+++ groups/models.py
@@ -22,7 +22,7 @@ class GeneralCategory(models.Model):
     icon = CloudinaryField('Ícone da categoria')
     creation_date = models.DateTimeField('Criado em', auto_now_add=True)
     last_updated_date = models.DateTimeField('Última modificação', auto_now=True)
-    
+
     class Meta:
         verbose_name = 'Categoria de Grupo'
         verbose_name_plural = 'Categorias de grupo'
@@ -38,7 +38,9 @@ class Group(models.Model):
     info = models.TextField('Info')
     general_category = models.ForeignKey('groups.GeneralCategory', verbose_name='Categoria', null=True, on_delete=models.SET_NULL)
     leader = models.ForeignKey('core.Member', verbose_name='Líder', related_name='group_leader', null=True, on_delete=models.SET_NULL)
+    another_leader = models.ForeignKey('core.Member', verbose_name='Outro líder', related_name='another_group_leader', null=True, on_delete=models.SET_NULL)
     vice_leader = models.ForeignKey('core.Member', verbose_name='Vice-líder', related_name='vice_leader', null=True, blank=True, on_delete=models.SET_NULL)
+    another_vice_leader = models.ForeignKey('core.Member', verbose_name='Outro vice-líder', related_name='another_vice_leader', null=True, on_delete=models.SET_NULL)
     third_leader = models.ForeignKey('core.Member', verbose_name='Terceiro líder', related_name='third_leader', null=True, blank=True, on_delete=models.SET_NULL)
     background_image = CloudinaryField('Imagem do grupo')
     church = models.ForeignKey('core.Church', verbose_name='Igreja', null=True, blank=True, on_delete=models.SET_NULL)
@@ -60,6 +62,7 @@ def group_background_image_delete(sender, instance, **kwargs):
 
 class GroupMeetingDate(models.Model):
     group = models.ForeignKey('groups.Group', verbose_name='Grupo', on_delete=models.CASCADE, related_name='meeting_dates')
+    title = models.CharField('Tipo do encontro', max_length=100, null=True, blank=True)
     day = models.CharField('Dia', choices=DAY_OPTIONS, max_length=20)
     start_date = models.TimeField('Início')
     end_date = models.TimeField('Fim', null=True, blank=True)
