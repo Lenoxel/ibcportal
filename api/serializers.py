621df84e09b1ca19d19618372f835b98a8f2aed0
Gabriel Lenon <glenonsilva@gmail.com>
2022-03-01 14:36:19 -0300

    :bug: Tenta resolver novamente get_presence_records

--- api/serializers.py
+++ api/serializers.py
@@ -202,7 +202,7 @@ class EBDLessonSerializer(serializers.ModelSerializer):
         return {
             'presents': EBDPresenceRecord.objects.filter(lesson__pk=obj.pk, attended = True).count(),
             'absents': EBDPresenceRecord.objects.filter(lesson__pk=obj.pk, attended = False).count(),
-            'pending_calls': EBDPresenceRecord.objects.values('ebd_class__name', 'attended').annotate(count=Count('ebd_class', distinct=True)).filter(lesson__pk=obj.pk, attended=False)
+            'pending_calls': (EBDPresenceRecord.objects.filter(lesson__pk=obj.pk, attended=False)).values('ebd_class__name').annotate(count=Count('ebd_class__name', distinct=True))
         }
 
     class Meta:
